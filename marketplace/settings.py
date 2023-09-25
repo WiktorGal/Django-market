edd282ef6e72f8cab15f2cd81bb5ca73c16bd860
WiktorGal <139254986+WiktorGal@users.noreply.github.com>
2023-09-25 21:49:26 +0200

    first commit

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-9c*f*lst1(!_z$$t+(&0)i_q*k42cp)!s5*odyyg6c+%&r()&w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 LOGIN_URL = '/login/'
@@ -133,3 +135,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
