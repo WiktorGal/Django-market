6e298546b5d2f3252eb4e5e8d0d2bb88d8f3ee16
WiktorGal <139254986+WiktorGal@users.noreply.github.com>
2023-09-25 22:07:42 +0200

    first commit

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9c*f*lst1(!_z$$t+(&0)i_q*k42cp)!s5*odyyg6c+%&r()&w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -127,8 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
