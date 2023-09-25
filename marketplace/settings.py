d13214144c31d796d89a52bae59f1af4d726f753
WiktorGal <139254986+WiktorGal@users.noreply.github.com>
2023-09-26 01:03:48 +0200

    image fix

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9c*f*lst1(!_z$$t+(&0)i_q*k42cp)!s5*odyyg6c+%&r()&w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = ['*']
 
@@ -131,6 +131,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 
 # Default primary key field type
