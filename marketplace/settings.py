cbe3a30e4430c02ff81d23ac9fb072d0c2a4130b
WiktorGal <139254986+WiktorGal@users.noreply.github.com>
2023-09-27 16:09:52 +0200

    Initial commit

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9c*f*lst1(!_z$$t+(&0)i_q*k42cp)!s5*odyyg6c+%&r()&w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 LOGIN_URL = '/login/'
